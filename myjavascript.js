f44cff190f5719b787cc57955af7ab9c5b11636c
Alessio Tiberi <alessio.1997.18.01@gmail.com>
2021-12-09 00:33:02 +0100

    fixato

--- myjavascript.js
+++ myjavascript.js
@@ -1,19 +1,31 @@
+// domande da fare all'utente 
 
 let yourKm = prompt ("Inserisci km da percorrere");
 let yourAge = prompt ("Inserisci la tua età");
+
+// operazione kmtot = costo al km per i km da percorrere
+
 let costoKmTot =(0.21 * yourKm) 
 
 if(yourAge <18){
 
-    costoKmTot = costoKmTot - ((costoKmTot / 100) * 20);
+    // 20% di sconto per i minorenni 
+
+    costoKmTot = costoKmTot - ((costoKmTot / 100) * 20); 
 
 }else if (yourAge >= 65){
 
-    costoKmTot = costoKmTot - ((costoKmTot / 100) * 20);
+    // 40% di sconto per gli over 65 
+
+    costoKmTot = costoKmTot - ((costoKmTot / 100) * 40);
     
 }
 
+// tofixed il numero affianco tra parentesi indica i decimali da prendere in considerazione
+
 costoKmTot = costoKmTot.toFixed(2)
+
+// valore in euro 
 
 document.getElementById("myprompt").innerHTML = costoKmTot + `€` ;
 
